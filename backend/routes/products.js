e38f899c1133dd5efabef959ef885e366f462cf7
KALU56 <kalkidanbehailu55@gmail.com>
2025-02-05 19:28:50 +0300

    change

--- backend/routes/products.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getAllProducts } = require('../controllers/productController');  // Import the controller
-
-// Route to get all products
-router.get('/', getAllProducts);
-
-module.exports = router;
